22b01b3686bbf7beb7ee1df21638c1067966d1eb
Shuvo Saha <sahashuvo@gmail.com>
2018-09-05 13:20:43 +0600

    Added classes Project Properties and Class Properties for storing various information about classes and the whole project. Need to brainstorm what more attributes do we want to store. Assumption: One class per file. LOC displayed here is incorrect, I am still working on resolving it.

--- Tool/src/FileReader/MyFileReader.java
+++ Tool/src/FileReader/MyFileReader.java
@@ -1,12 +1,91 @@
 package FileReader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import CodeStructure.ClassProperties;
+
 public class MyFileReader {
+	
 	private ArrayList<String> fileList;
 	
+	private String projectDirectory;
+	
+	// Author: Shuvo Saha
+	public MyFileReader (String currentProjectDirectory) {
+		this.projectDirectory = currentProjectDirectory;
+	}
+	
+	// Author: Shuvo Saha
+	public ArrayList<String> listOfPackageFolders (){
+		String projectSource = projectDirectory+"\\src";
+		ArrayList<String> packageFolders = new ArrayList<String>();
+		File folder = new File(projectSource);
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+			if(file.isDirectory()) {
+				packageFolders.add(file.getName());
+			}
+		}
+		return packageFolders;
+	}
+	
+	// Author: Shuvo Saha
+	private boolean isJavaFile (String fileName) {
+		
+		String extension = "";
+		int i = fileName.lastIndexOf('.');
+		if (i > 0) {
+		    extension = fileName.substring(i+1);
+		}
+		
+		return extension.equals("java");
+	}
+	
+	// Author: Shuvo Saha
+	public ClassProperties readJavaFile (String packageName,String fileName) {
+		
+		String rawCode = "";
+		try {
+			rawCode = new String(Files.readAllBytes(Paths.get(projectDirectory+"\\src\\"+packageName+"\\"+fileName)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Eguli ektao successfully white space remove kore nai
+		rawCode.replaceAll("\\s+","");
+		rawCode.trim();
+		//////////////////////////////////////////////////////
+		
+		Integer loc = (int) rawCode.chars().filter(ch -> ch ==';').count();
+		String className = fileName.split("\\.")[0];
+		return new ClassProperties(loc,rawCode,packageName,className);
+	}
+	
+	// Author: Shuvo Saha
+	public ArrayList<ClassProperties> getClassPropertiesOfPackageFiles(String packageName){
+		ArrayList<ClassProperties> classPropertiesOfFilesInPackage = new ArrayList<ClassProperties>();
+		
+		String packageSource = projectDirectory+"\\src\\"+packageName;
+		File folder = new File(packageSource);
+		File[] listOfFiles = folder.listFiles();
+		
+		for (File file : listOfFiles) {
+			if(!file.isDirectory()) {
+				if(isJavaFile(file.getName())) {
+					classPropertiesOfFilesInPackage.add(readJavaFile(packageName, file.getName()));
+				}
+			}
+		}
+		return classPropertiesOfFilesInPackage;
+	}
+	
+	// Author: Aquib Azmain
 	private void listFilesForFolder(File folder) {
 	    for (File fileEntry : folder.listFiles()) {
 	        if (fileEntry.isDirectory()) {
@@ -21,6 +100,7 @@ public class MyFileReader {
 	    }
 	}
 	
+	// Author: Aquib Azmain
 	public ArrayList<String> manageFileReader() {
 		String projectName = "";
 		System.out.println("Enter folder name: ");
--- Tool/src/Manager/MyMain.java
+++ Tool/src/Manager/MyMain.java
@@ -3,14 +3,26 @@ package Manager;
 import java.io.File;
 import java.util.ArrayList;
 
+import CodeStructure.ProjectProperties;
 import FileReader.MyFileReader;
 
 public class MyMain {
 
 	public static void main(String[] args) {
-		ArrayList<String> fileList;
-		MyFileReader myFileReader = new MyFileReader();
-		fileList = myFileReader.manageFileReader();
+		
+		//ArrayList<String> fileList;
+		ProjectProperties mySceneryProperties = new ProjectProperties();
+		
+		//Please change project directory before running this
+		MyFileReader myFileReader = new MyFileReader("E:\\Object-Oriented-Metrics-Tool\\MyScenery");
+		
+		mySceneryProperties.packages = myFileReader.listOfPackageFolders();
+		for (String packageName : mySceneryProperties.packages) {
+			mySceneryProperties.classProperties.addAll(myFileReader.getClassPropertiesOfPackageFiles(packageName));
+		}
+		mySceneryProperties.printAllProjectProperties();
+		
+		//fileList = myFileReader.manageFileReader();
 	}
 
 }
