d9f1ab6c0eaf9cd9acc6cfbb22d3816ac5009978
Syed Fatiul Huq (Sujoy) <s.f.huq11@gmail.com>
2018-09-15 16:05:59 +0600

    method statement extraction

--- Tool/src/cohesion/LOCMCalculation.java
+++ Tool/src/cohesion/LOCMCalculation.java
@@ -3,7 +3,13 @@ package cohesion;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
@@ -28,11 +34,16 @@ public class LOCMCalculation {
             populateMethods(compilationUnit);
             populateInstanceFields(compilationUnit);
 
-        } catch (ParseException e) {
-            e.printStackTrace();
+//            exploreMethods(compilationUnit);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void exploreMethods(CompilationUnit compilationUnit) {
+        MethodVariableCollector methodVariableCollector = new MethodVariableCollector();
+        methodVariableCollector.visit(compilationUnit, null);
     }
 
     private void populateInstanceFields(CompilationUnit compilationUnit) {
@@ -62,8 +73,45 @@ class MethodNameCollector extends VoidVisitorAdapter<List<String>>{
     @Override
     public void visit(MethodDeclaration md, List<String> collector) {
         super.visit(md, collector);
+        System.out.println("\n" + md.getName());
+        collector.add(md.getDeclarationAsString());
 
-        collector.add(md.getName());
+        MethodVariableCollector methodVariableCollector = new MethodVariableCollector();
+        methodVariableCollector.visit(md, null);
+
+        VariableFetcher variableFetcher = new VariableFetcher();
+        variableFetcher.visit(md, null);
+
+        AssignExprFetcher assignExprFetcher = new AssignExprFetcher();
+        assignExprFetcher.visit(md, null);
+
+        VariableDeclarationFetcher variableDeclarationFetcher = new VariableDeclarationFetcher();
+        variableDeclarationFetcher.visit(md, null);
+    }
+}
+
+class AssignExprFetcher extends VoidVisitorAdapter<Void>{
+
+    public void visit(AssignExpr ae, Void v){
+        System.out.println("ae " + ae.toString() + " ");
+    }
+}
+
+class VariableDeclarationFetcher extends VoidVisitorAdapter<Void>{
+
+    public void visit(VariableDeclarationExpr vd, Void v){
+        System.out.println("vd " + vd.toString());
+        List<VariableDeclarator> variableDeclarators = vd.getVariables();
+        for(VariableDeclarator variableDeclarator: variableDeclarators){
+            System.out.println("    " + variableDeclarator.getName());
+        }
+    }
+}
+
+class VariableFetcher extends VoidVisitorAdapter<Void>{
+
+    public void visit(MethodCallExpr mce, Void v){
+        System.out.println("mce " + mce.toString() + mce.getScope());
     }
 }
 
@@ -75,6 +123,21 @@ class InstanceFieldNameCollector extends VoidVisitorAdapter<List<String>>{
 
         for(VariableDeclarator vd: fd.getVariables()){
             collector.add(vd.toString());
+        }
+    }
+}
+
+class MethodVariableCollector extends VoidVisitorAdapter<Void>{
+
+    @Override
+    public void visit(MethodDeclaration md, Void v) {
+        super.visit(md, v);
+
+        List<Parameter> parameters = md.getParameters();
+        if(parameters != null){
+            for(Parameter parameter: parameters){
+                System.out.println(md.getName() + ": " + parameter.toString());
+            }
         }
     }
 }
--- Tool/src/cohesion/TestClass.java
+++ Tool/src/cohesion/TestClass.java
@@ -3,15 +3,19 @@ package cohesion;
 public class TestClass {
     int a, b ,c ,d;
 
-    void x(){
+    void x(int w){
         a = a + 1;
+        int y = a;
+        Math.abs(a);
     }
 
-    void y(){
-        a = b + 1;
+    void y(int x, int y){
+        a = b + x;
+        x(a);
     }
 
     void z(){
         System.out.println(d);
+        System.out.println(b);
     }
 }
